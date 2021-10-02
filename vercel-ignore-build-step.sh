5440f265938d791b83055207038bca36573d9148
Shutarou Takayama <hi.mountain110916@gmail.com>
2021-10-02 22:32:27 +0900

    shell修正依頼

--- vercel-ignore-build-step.sh
+++ vercel-ignore-build-step.sh
@@ -3,7 +3,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "develop"  ]] ; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "develop" || "$VERCEL_ENV" == "production" ]] ; then
   # Proceed with the build
     echo "✅ - Build can proceed"
   exit 1;
